c2ef5e45a8fd7f648efd853c229849097b1bcd88
nishide <nishide.h@gmail.com>
2018-03-05 10:03:14 +0900

    update "@user作成し、コントローラーの値を表示"

--- mitakalab-twitter/app/controllers/users_controller.rb
+++ mitakalab-twitter/app/controllers/users_controller.rb
@@ -3,5 +3,10 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = Hash.new
+    @user[:name] = 'Takahiro Aoki'
+    @user[:username] = 'Hanryu'
+    @user[:location] = 'Tokyo, Japan'
+    @user[:about] = 'Nice to meet you!!'
   end
 end
