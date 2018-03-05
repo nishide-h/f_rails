64d0c9f3e34d2eff584401cead3bdd05022e1fbf
nishide <nishide.h@gmail.com>
2018-03-05 10:15:44 +0900

    update 'urlから受け取ったパラメータによる分岐処理'

--- mitakalab-twitter/app/controllers/users_controller.rb
+++ mitakalab-twitter/app/controllers/users_controller.rb
@@ -4,9 +4,16 @@ class UsersController < ApplicationController
 
   def show
     @user = Hash.new
-    @user[:name] = 'Takahiro Aoki'
-    @user[:username] = 'Hanryu'
-    @user[:location] = 'Tokyo, Japan'
-    @user[:about] = 'Nice to meet you!!'
+    if params[:username] == 'Hirro'
+      @user[:name] = 'Hiro Nishide'
+      @user[:username] = 'Hirro'
+      @user[:location] = 'Kyoto, Japan'
+      @user[:about] = 'Hello, I am Hiro.'
+    elsif params[:username] == 'Hanryu'
+      @user[:name] = 'Takahiro Aoki'
+      @user[:username] = 'Hanryu'
+      @user[:location] = 'Tokyo, Japan'
+      @user[:about] = 'Nice to meet you!!'
+    end
   end
 end
