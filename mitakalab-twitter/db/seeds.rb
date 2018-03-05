127c5fe61b849ec35c5e88154c16661bbdaccbcc
nishide <nishide.h@gmail.com>
2018-03-05 10:56:54 +0900

    add 'seedファイルにDBデータを定義'

--- mitakalab-twitter/db/seeds.rb
+++ mitakalab-twitter/db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+@user = User.new
+@user.name = 'Hiro Nishide'
+@user.username = 'hirooo'
+@user.location = 'Kyoto, Japan'
+@user.about = 'Hello, I am Hiro. From database!'
+@user.save
+
+@user = User.new
+@user.name = 'Takahiro Aoki'
+@user.username = 'hanryu'
+@user.location = 'Tokyo, Japan'
+@user.about = "Nice to meet you!! I\'m from database!"
+@user.save
