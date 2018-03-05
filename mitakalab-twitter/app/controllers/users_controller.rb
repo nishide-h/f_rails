7935fe139283cc770b8a7d1f6806adb3ba264f3d
nishide <nishide.h@gmail.com>
2018-03-05 11:14:43 +0900

    update "パラメータからDBデータを取得し表示"

--- mitakalab-twitter/app/controllers/users_controller.rb
+++ mitakalab-twitter/app/controllers/users_controller.rb
@@ -3,17 +3,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = Hash.new
-    if params[:username] == 'Hirro'
-      @user[:name] = 'Hiro Nishide'
-      @user[:username] = 'Hirro'
-      @user[:location] = 'Kyoto, Japan'
-      @user[:about] = 'Hello, I am Hiro.'
-    elsif params[:username] == 'Hanryu'
-      @user[:name] = 'Takahiro Aoki'
-      @user[:username] = 'Hanryu'
-      @user[:location] = 'Tokyo, Japan'
-      @user[:about] = 'Nice to meet you!!'
-    end
+      @user = User.find_by(username: params[:username])
   end
 end
