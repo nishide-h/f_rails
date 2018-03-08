53775d0dde4583a760805ba66d1dca4a6e44fe14
nishide <nishide.h@gmail.com>
2018-03-08 13:35:05 +0900

    update "DB seedのtweetデータ追加"

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
+# coding: utf-8
 
 @user = User.new
 @user.name = "Hiro Nishide"
@@ -12,4 +13,7 @@
 @user.username = "hanryu"
 @user.location = "Tokyo, Japan"
 @user.about = "Nice to meet you!! I\'m from database!"
-@user.save
+@user.save 
+100.times do |count|
+  Tweet.create(title: "タイトル #{ count }", content: "コメント #{ count }")
+end
