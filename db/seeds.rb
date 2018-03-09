dddf2ade03a915b9a9f89ee08de0c06928bd56b5
nishide <nishide.h@gmail.com>
2018-03-09 10:16:28 +0900

    update "db/seeds.rbの既存ユーザーがdeviseの邪魔になるのでコメントアウト"

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: false
 # coding: utf-8
 
-@user = User.new
-@user.name = "Hiro Nishide"
-@user.username = "hirooo"
-@user.location = "Kyoto, Japan"
-@user.about = "Hello, I am Hiro. From database!"
-@user.save
-
-@user = User.new
-@user.name = "Takahiro Aoki"
-@user.username = "hanryu"
-@user.location = "Tokyo, Japan"
-@user.about = "Nice to meet you!! I\'m from database!"
-@user.save
+# @user = User.new
+# @user.name = "Hiro Nishide"
+# @user.username = "hirooo"
+# @user.location = "Kyoto, Japan"
+# @user.about = "Hello, I am Hiro. From database!"
+# @user.save
+#
+# @user = User.new
+# @user.name = "Takahiro Aoki"
+# @user.username = "hanryu"
+# @user.location = "Tokyo, Japan"
+# @user.about = "Nice to meet you!! I\'m from database!"
+# @user.save
 100.times do |count|
   Tweet.create(title: "タイトル #{ count }", content: "コメント #{ count }")
 end
