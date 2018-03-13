7739cee01d0eb30482bbe53ca7bacf2178b3e6bd
nishide <nishide.h@gmail.com>
2018-03-13 17:07:14 +0900

    add "通常サインアップ時のuidカラム設定"

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+  def self.create_unique_string
+    SecureRandom.uuid
+  end
 end
