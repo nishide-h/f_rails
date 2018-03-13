6c459277455a55ab308e5527be8c90d80f0c2add
nishide <nishide.h@gmail.com>
2018-03-13 16:59:35 +0900

    add "OAuthのcallback処理"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
   root to: "tweets#index"
   get "tweets/index"
   get "tweets/show"
