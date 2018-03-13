8c47270e51af059330a6404eec9d04b450bd9204
nishide <nishide.h@gmail.com>
2018-03-13 17:08:37 +0900

    add "OAuth認証時のemail設定"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,27 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
+  def self.find_for_twitter_oauth(auth, signed_in_resorce = nil)
+    user = User.where(provider: auth.provider, uid: auth.uid).first
+    unless user
+      user = User.create(name: auth.info.nickname,
+                         provider: auth.provider,
+                         uid: auth.uid,
+                         email: User.create_unique_email,
+                         password: Devise.friendly_token[0, 20]
+                        )
+    end
+    user
+  end
+
   def self.create_unique_string
     SecureRandom.uuid
+  end
+
+  def self.create_unique_email
+    User.create_unique_string + "@example.com"
   end
 end
