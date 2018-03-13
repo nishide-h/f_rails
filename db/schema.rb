3befe00ad1d724c611fea6c02651188a46217669
nishide <nishide.h@gmail.com>
2018-03-13 16:50:38 +0900

    add "UserモデルにOmniAuthで使用するカラムを追加"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180309010936) do
+ActiveRecord::Schema.define(version: 20180313014756) do
 
   create_table "tweets", force: :cascade do |t|
     t.string "title"
@@ -36,8 +36,11 @@ ActiveRecord::Schema.define(version: 20180309010936) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "provider", default: "", null: false
+    t.string "uid", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
 end
