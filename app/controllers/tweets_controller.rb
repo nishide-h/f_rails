1546d25ea309c37814b9f7566a088c88cc8a8092
nishide <nishide.h@gmail.com>
2018-03-26 17:23:05 +0900

    add "Tweetコントローラーshow実装"

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class TweetsController < ApplicationController
   PER = 5
 
@@ -7,13 +5,21 @@ class TweetsController < ApplicationController
     @tweets = Tweet.page(params[:page]).per(PER)
   end
 
-  def show; end
+  def show
+    @tweet = Tweet.where(id: :id, user_id: :user_id)
+  end
 
   def new; end
 
   def create
     Tweet.create(title: params[:tweet][:title],
-                 content: params[:tweet][:content])
+                 content: params[:tweet][:content],
+                 user_id: params[:tweet][:user_id])
     redirect_to "/tweets/index"
   end
+
+  private
+    def tweet_params
+      params.require(:tweet).permit(:id, :title, :content, :user_id)
+    end
 end
