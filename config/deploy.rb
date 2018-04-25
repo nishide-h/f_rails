28a4bbde73b1f965d0f1613038608b4d72d2bf9d
nishide <nishide.h@gmail.com>
2018-04-25 16:10:08 +0900

    add "デプロイ設定を追記"

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,26 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.10.1"
+lock "3.10.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "f_rails"
+set :repo_url, "git@github.com:nishide-h/f_rails.git"
+
+# task :update do
+#   run_locally do
+#     application = fetch :application
+#     if test "[ -d #{application} ]"
+#       execute "cd #{application}; git pull"
+#     else
+#       execute "git clone #{fetch :repo_url} #{application}"
+#     end
+#   end
+# end
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/nishide/public_html/nishide.tk/public/f_rails"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -18,7 +30,7 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true # sudoが実行できるように
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
@@ -37,3 +49,28 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+set :ssh_options, :port =&amp;gt; "4649"
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1'
+
+#気にしない↓↓↓
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
+
+set :linked_dirs, %w{bin log tmp/backup tmp/pids tmp/cache tmp/sockets vendor/bundle}
+set :unicorn_pid, "#{shared_path}/tmp/pids/unicorn.pid"
+
+set :bundle_jobs, 4
+
+
+
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
+# Default
