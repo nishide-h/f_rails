c09396e7b1b3409b0b69e8652e29b7d7556a9db5
nishide <nishide.h@gmail.com>
2018-04-28 21:56:47 +0900

    update "rbenvのパスをバージョン入れて指定"

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,7 +55,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
 set :rbenv_path, "~/.rbenv"
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_prefix, "#{fetch(:rbenv_path)}/versions/2.5.1/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all
 
